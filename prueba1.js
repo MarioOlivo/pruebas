addb0de0082b64c7de40f618ff3e08d0e26b1a33
Mario Olivo <106557614+MarioOlivo@users.noreply.github.com>
2022-08-28 21:35:24 -0600

    calculadora

--- prueba1.js
+++ prueba1.js
@@ -1,1 +1,33 @@
 //askdnfknasd asd asdk mas
+
+
+console.log(fnaritmeticas('+',9,2));
+function fnaritmeticas(modo,v1,v2) {
+    let resultado;
+    if (typeof(v1)=='number' && typeof(v2)=='number') {
+        
+    switch (modo) {
+        case '+':
+            resultado = Number(v1)+Number(v2)
+            break;
+        case '-':
+            resultado = Number(v1)-Number(v2)
+            break;
+        case '*':
+            resultado = Number(v1)*Number(v2)
+            break;
+        case '/':
+            resultado = Number(v1)/Number(v2)
+            break;
+        case '**':
+            resultado = Number(v1)**Number(v2)
+            break;
+        default:
+            break;
+    }
+    } else{
+        resultado ='Los valores deben ser numeros'
+    }
+    return resultado;
+}
+
